4fdd722f068194ff736fa6c627029ced6a87e076
iClass <iclasschima@gmail.com>
2020-07-11 14:10:04 +0100

    working with date and time

--- src/com/account/AirlineReservation.java
+++ src/com/account/AirlineReservation.java
@@ -1,15 +1,18 @@
 package com.account;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
 
 public class AirlineReservation {
 
     public static void main(String[] args) {
         boolean[] seats = new boolean[10];
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         Scanner input = new Scanner(System.in);
         int response;
         int economySeatsCount = 1;
         int firstClassSeatsCount = 1;
-
 
         while (true) {
             System.out.print("Please enter 1 for FIRST-CLASS or Enter 2 for ECONOMY or 0 to quit: ");
@@ -25,11 +28,15 @@ public class AirlineReservation {
                     if (seats[i] == false) {
                         seats[i] = true;
                         firstClassSeatsCount++;
-                        System.out.printf("Your seat number is: %d [FIRST-CLASS] %n", i + 1);
+                        System.out.println("Seat booked Successful!");
+                        System.out.println("\nBoarding Pass");
+                        System.out.printf("Seat number: %d [FIRST-CLASS] %n", i + 1);
+                        System.out.println("Date: " + date);
+                        System.out.println("Time: " + LocalTime.now().format(myFormatter) + "\n");
                         break;
                     }
                     if (firstClassSeatsCount > 5) {
-                        System.out.println("FIRST-CLASS section is full. Enter 2 to be placed in ECONOMY section");
+                        System.out.println("FIRST-CLASS section is full. Enter 2 to be placed in ECONOMY section\n");
                         break;
                     }
                 }
@@ -39,7 +46,11 @@ public class AirlineReservation {
                     if (seats[i] == false) {
                         seats[i] = true;
                         economySeatsCount++;
-                        System.out.printf("Your seat number is: %d [ECONOMY] %n", i + 1);
+                        System.out.println("Seat booked Successful!");
+                        System.out.println("\nBoarding Pass");
+                        System.out.printf("Seat number: %d [ECONOMY] %n", i + 1);
+                        System.out.println("Date: " + date);
+                        System.out.println("Time: " + LocalTime.now().format(myFormatter) + "\n");
                         break;
                     }
                     if (economySeatsCount > 5) {
@@ -47,6 +58,8 @@ public class AirlineReservation {
                         break;
                     }
                 }
+            } else {
+                System.out.println("Invalid command entered!\n");
             }
 
             if (economySeatsCount > 5 && firstClassSeatsCount > 5) {
